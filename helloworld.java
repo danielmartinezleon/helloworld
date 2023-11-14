6f29e7eda32f2d3cde375533df0daba92177e006
danielmartinezleon <122262363+danielmartinezleon@users.noreply.github.com>
2023-11-14 14:00:37 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld{
     public static void main(String[]args){
-        System.out.println("*".repeat(10));
-        System.out.println("Hello World");
-        System.out.println("*".repeat(10));
+        System.out.println("*".repeat(14));
+        System.out.println("* Hello World *");
+        System.out.println("*".repeat(14));
     }
 }
