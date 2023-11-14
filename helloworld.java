fb05d486dfbd17e94752207c5dc7fa662e9d93f0
Daniel Martínez León <daniel1martinezleon@gmail.com>
2023-11-14 14:02:06 +0100

    ahora en español

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld{
     public static void main(String[]args){
-        System.out.println("*".repeat(10));
-        System.out.println("Hello World");
-        System.out.println("*".repeat(10));
+        System.out.println("*".repeat(11));
+        System.out.println("* Hola Mundo *");
+        System.out.println("*".repeat(11));
     }
 }
